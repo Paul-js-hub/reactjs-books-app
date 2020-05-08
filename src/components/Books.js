53bf16dc0719cfaf95e6b5cf69d51c42ac30fd61
Paul Onchera <hesbonpoisson@gmail.com>
2020-05-08 13:25:14 +0300

    Adding a resource to the backend

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,44 +5,49 @@ import axios from 'axios';
 class Books extends Component {
     state = {
         books: [],
-        book:{}
+        title: ""
     }
 
-    componentDidMount = () =>{
+    componentDidMount() {
+        this.fetchBooks();
+    }
+
+    fetchBooks = () => {
         axios.get('http://localhost:50/api/books')
-        .then(res => {
-            //console.log('response', res)
-            this.setState({res});
-        });
+            .then(res => {
+                console.log('response', res)
+                this.setState({ books:res.data });
+            });
     }
 
 
-    handleAddbook = () =>{
-        const newBook = this.state.book;
-        const books = this.state.books;
-        books.push(newBook);
-        this.setState({books})
-        console.log("hey we are here")
+    handleAddbook = () => {
+        const {title} = this.state;
+        console.log("title", title)
+        axios.post('http://localhost:50/api/books', {title}, {
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+
     }
 
-    onChange=(event)=>{
-        const title = event.target.value || "";
-        const book = { id:45, title}
-        this.setState({book});
+    onChange = (event) => {
+        this.setState({ title: event.target.value })
     }
 
     render() {
         const books = this.state.books;
         return (<div>{
-            books.map((book) =>{
-            return(<Book key={book.id} title={book.title}/>);
-        })}
-        <div>
-            <input name="title" onChange={this.onChange}/>
-            <button type="button" onClick={this.handleAddbook}>Add book</button>
-         </div>
+            books.map((book) => {
+                return (<Book key={book.id} title={book.title} />);
+            })}
+            <div>
+                <input name="title" onChange={(event) =>this.onChange(event)} />
+                <button type="button" onClick={this.handleAddbook}>Add book</button>
+            </div>
         </div>)
-        
+
     }
 
 }
