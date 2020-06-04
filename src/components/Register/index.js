3a4a564511fd1298342f0c2c1f9863b17da8bf34
Paul Onchera <hesbonpoisson@gmail.com>
2020-06-04 10:58:23 +0300

    installed font awesome icons
    added the font awesome to the code

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import './register.css'
 import axios from "axios";
+import { Link } from "react-router-dom";
+import {
+  faGithubAlt,
+  faGoogle,
+  faFacebook,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 class Register extends Component {
   state = {
@@ -24,6 +33,13 @@ class Register extends Component {
   render() {
     return (
         <div class="form-container">
+          <h1 className="header-registration">Register for an account</h1>
+          <div className="social-container">
+          <Link><FontAwesomeIcon icon={faGoogle} style={{ color: 'red' }} className="google-icon" /></Link>
+          <Link><FontAwesomeIcon icon={faGithubAlt} style={{ color: 'gray' }} className="github-icon"/></Link>
+          <Link><FontAwesomeIcon icon={faFacebook} style={{ color: 'blue' }} className="facebook-icon"/></Link>
+          <Link><FontAwesomeIcon icon={faTwitter} style={{ color: 'light-blue' }} className="twitter-icon"/></Link>
+          </div>
           <input class="form-input" name="email" placeholder="Enter your email" type="text" onChange={(e) => this.onChange(e)} />
           <input class="form-input" name="password" placeholder="Enter password" type="password" onChange={(e) => this.onChange(e)} />
           <button className="sumbit-btn" onClick={this.onSubmit}>Register</button>
