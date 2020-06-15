7db713984f84af974bf237b03cdfbacbb9681821
Paul Onchera <hesbonpoisson@gmail.com>
2020-06-15 16:47:22 +0300

    more styling to the addbook popup

--- src/components/AddBook/index.js
+++ src/components/AddBook/index.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import Popup from 'reactjs-popup';
-import './addbook.css';
+import React, { Component } from "react";
+import Popup from "reactjs-popup";
+import "./addbook.css";
 //import axios from 'axios';
 
 export class AddBook extends Component {
   state = {
     open: "false",
-    title: '',
-    author: '',
-    bookImage: ''
-  }
+    title: "",
+    author: "",
+    bookImage: "",
+  };
 
   onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   addBook = () => {
     const { author, title, bookImage } = this.state;
     const { handleAddbook } = this.props; // parent prop function
     handleAddbook({ title, author, bookImage }); // call parent prop function
+  };
 
-  }
-
-  fileSelectedHandler = event => {
-    console.log(event.target.files[0])
+  fileSelectedHandler = (event) => {
+    console.log(event.target.files[0]);
     this.setState({ bookImage: event.target.files[0] });
-  }
+  };
 
   openModal() {
     this.setState({ open: "true" });
@@ -33,52 +32,58 @@ export class AddBook extends Component {
   closeModal() {
     this.setState({ open: "false" });
   }
+
   render() {
     const { title, author, bookImage } = this.state;
     const { handleAddbook } = this.props;
     return (
       <div className="addbook-container">
         <Popup
-          trigger=
-          {<button className="addbook-btn">
-            <i class="fa fa-plus my-float"></i>
-          </button>}
+          trigger={
+            <button className="addbook-btn">
+              <i class="fa fa-plus my-float"></i>
+            </button>
+          }
           position="left bottom"
           arrow={false}
         >
-          {close => (
+          {(close) => (
             <div className="popup-container">
               <div className="add-book-header">
                 <h3>Add book</h3>
               </div>
-              <input
-                className="add-book-input-details"
-                type='text'
-                name='title'
-                placeholder='Enter the title of the book'
-                onChange={(e) => this.onChange(e)}
-              >
-              </input>
-              <input
-                className="add-book-input-details"
-                type='text'
-                name='author'
-                placeholder='Enter the author of the book'
-                onChange={(e) => this.onChange(e)}
-              >
-              </input>
+              <div className="add-book-input-container">
+                <input
+                  className="add-book-input-details"
+                  type="text"
+                  name="title"
+                  placeholder="Enter the title"
+                  onChange={(e) => this.onChange(e)}
+                ></input>
+                <input
+                  className="add-book-input-details"
+                  type="text"
+                  name="author"
+                  placeholder="Enter the author"
+                  onChange={(e) => this.onChange(e)}
+                ></input>
+              </div>
               <div className="input-file-wrapper">
                 <input
                   className="upload-bookimage"
                   type="file"
                   name="bookImage"
-                  onChange={this.fileSelectedHandler}>
-                </input>
+                  onChange={this.fileSelectedHandler}
+                ></input>
               </div>
               <div className="button-group">
-                <button className="btn-close" onClick={close} >Cancel</button>
-                <button className="button-save"
-                  onClick={() => handleAddbook({ title, author, bookImage })}>
+                <button className="btn-close" onClick={close}>
+                  Cancel
+                </button>
+                <button
+                  className="button-save"
+                  onClick={() => handleAddbook({ title, author, bookImage })}
+                >
                   Save
                 </button>
               </div>
@@ -86,11 +91,8 @@ export class AddBook extends Component {
           )}
         </Popup>
       </div>
-
-    )
+    );
   }
 }
 
-
-
-export default AddBook
+export default AddBook;
--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
-import EditBook from '../EditBook/index';
-import './book.css';
-import { MDBIcon } from 'mdbreact';
+import React, { Component } from "react";
+import EditBook from "../EditBook/index";
+import "./book.css";
+import { MDBIcon } from "mdbreact";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export class Book extends Component {
-    render() {
-        const { onDelete } = this.props;
-        const title = this.props.title || '';
-        const author = this.props.author || '';
-        const bookImage = this.props.bookImage || '';
-        const id = this.props.id || '';
+  render() {
+    const { onDelete } = this.props;
+    const title = this.props.title || "";
+    const author = this.props.author || "";
+    const bookImage = this.props.bookImage || "";
+    const id = this.props.id || "";
 
-        return (
-            <div className='card'>
-                <div className="delete-container">
-                    <button className="btn-delete" onClick={() => onDelete(id)}><MDBIcon far icon="trash-alt" /></button>
-                </div>
-                <EditBook
-                    onEdit={this.props.onEdit}
-                    id={id}
-                    title={title}
-                />
-                <div className='image'>
-                    <img src={bookImage} alt="JavaScript" width="185" height="185" />
-                </div>
-                <div className="content-title">{title}</div>
-                <div className="content-author">by:{author}</div>
-            </div>
-
-        )
-    }
+    return (
+      <div className="card">
+        <div className="delete-container">
+          <button className="btn-delete" onClick={() => onDelete(id)}>
+            <MDBIcon far icon="trash-alt" />
+          </button>
+        </div>
+        <EditBook onEdit={this.props.onEdit} id={id} title={title} />
+        <div className="image">
+          <img src={bookImage} alt="JavaScript" width="185" height="185" />
+        </div>
+        <div className="content-title">{title}</div>
+        <div className="content-author">by:{author}</div>
+      </div>
+    );
+  }
 }
-
-
-
 
 export default Book;
