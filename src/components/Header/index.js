f7f2e0b82128fd50bbf1bbfc050b04b42f0309fb
Paul Onchera <hesbonpoisson@gmail.com>
2020-06-18 16:43:07 +0300

    added ant design ui

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,73 +1,131 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import "./header.css";
 
-const Header = () => (
-  <div>
-    <nav className="sidebar-navigation">
-      <div id="menuToggle">
-        <input type="checkbox"></input>
+import { Layout, Menu } from "antd";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
 
-        <span></span>
-        <span></span>
-        <span></span>
+const { SubMenu } = Menu;
+const { Header, Sider } = Layout;
 
-        <ul id="menu">
-          <Link to="/">
-            <li>HOME</li>
-          </Link>
-          <Link to="/about">
-            <li>ABOUT</li>
-          </Link>
-          <Link to="/books">
-            <li>BOOKS</li>
-          </Link>
-          <Link to="/contact">
-            <li>CONTACT</li>
-          </Link>
-          <Link to="/register">
-            <li>REGISTER</li>
-          </Link>
-          <Link to="/login">
-            <li>LOGIN</li>
-          </Link>
-        </ul>
-      </div>
-    </nav>
+const NavHeader = () => (
+  <Layout>
     <div id="main" className="header-container">
-        <li className="nav-item">
-          <Link className="text-link" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="text-link" to="/about">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="text-link" to="/books">
-            Books
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="text-link" to="/contacts">
-            Contact
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="text-link" to="/register">
-            Register
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="text-link" to="/login">
-            Login
-          </Link>
-        </li>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+        <Menu.Item key="1">
+          <li className="nav-item">
+            <Link className="text-link" to="/">
+              Home
+            </Link>
+          </li>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <li className="nav-item">
+            <Link className="text-link" to="/about">
+              About
+            </Link>
+          </li>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <li className="nav-item">
+            <Link className="text-link" to="/books">
+              Books
+            </Link>
+          </li>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <li className="nav-item">
+            <Link className="text-link" to="/contacts">
+              Contact
+            </Link>
+          </li>
+        </Menu.Item>
+        <Menu.Item key="5">
+          <li className="nav-item">
+            <Link className="text-link" to="/register">
+              Register
+            </Link>
+          </li>
+        </Menu.Item>
+        <Menu.Item key="6">
+          <li className="nav-item">
+            <Link className="text-link" to="/login">
+              Login
+            </Link>
+          </li>
+        </Menu.Item>
+      </Menu>
+    </Header>
     </div>
-  </div>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%", borderRight: 0 }}
+        >
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+    </Layout>
+  </Layout>
+
+  // <div id="main" className="header-container">
+  //     <li className="nav-item">
+  //       <Link className="text-link" to="/">
+  //         Home
+  //       </Link>
+  //     </li>
+  //     <li className="nav-item">
+  //       <Link className="text-link" to="/about">
+  //         About
+  //       </Link>
+  //     </li>
+  //     <li className="nav-item">
+  //       <Link className="text-link" to="/books">
+  //         Books
+  //       </Link>
+  //     </li>
+  //     <li className="nav-item">
+  //       <Link className="text-link" to="/contacts">
+  //         Contact
+  //       </Link>
+  //     </li>
+  //     <li className="nav-item">
+  //       <Link className="text-link" to="/register">
+  //         Register
+  //       </Link>
+  //     </li>
+  //     <li className="nav-item">
+  //       <Link className="text-link" to="/login">
+  //         Login
+  //       </Link>
+  //     </li>
+  // </div>
 );
 
-export default Header;
+export default NavHeader;
