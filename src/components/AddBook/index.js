0848fc0ca5963c3aa622f3570c55eeab684bd219
Paul Onchera <hesbonpoisson@gmail.com>
2020-06-21 12:58:50 +0300

    removed the popup

--- src/components/AddBook/index.js
+++ src/components/AddBook/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import Popup from "reactjs-popup";
 import "./addbook.css";
 import axios from "axios";
 import { toast } from "react-smart-toaster";
 
 export class AddBook extends Component {
   state = {
-    open: "false",
     title: "",
     author: "",
     bookImage: "",
@@ -24,22 +22,23 @@ export class AddBook extends Component {
     fd.append("bookImage", bookImage);
     fd.append("title", title);
     fd.append("author", author);
-    axios.post(process.env.REACT_APP_API_URL + "/books", fd, {
-      headers: {
-        "auth-token": token,
-      },
-    })
-    .then((response)=>{
-      const message = response.data.message;
-      toast.success(message);
-      if (message === "Your book has been successfully uploaded") {
-        this.props.history.push("/");
-      }
-    })
-    .catch((err)=>{
-      const message = err.response.data.message;
-      toast.error(message);
-    })
+    axios
+      .post(process.env.REACT_APP_API_URL + "/books", fd, {
+        headers: {
+          "auth-token": token,
+        },
+      })
+      .then((response) => {
+        const message = response.data.message;
+        toast.success(message);
+        if (message === "Your book has been successfully uploaded") {
+          this.props.history.push("/");
+        }
+      })
+      .catch((err) => {
+        const message = err.response.data.message;
+        toast.error(message);
+      });
   };
 
   fileSelectedHandler = (event) => {
@@ -47,67 +46,46 @@ export class AddBook extends Component {
     this.setState({ bookImage: event.target.files[0] });
   };
 
-  openModal() {
-    this.setState({ open: "true" });
-  }
-  closeModal() {
-    this.setState({ open: "false" });
-  }
-
   render() {
     return (
       <div className="addbook-container">
-        <Popup
-          trigger={
-            <button className="addbook-btn">
-              <i className="fa fa-plus my-float"></i>
+        <div className="popup-container">
+          <div className="add-book-header">
+            <h3>Add book</h3>
+          </div>
+          <div className="add-book-input-container">
+            <input
+              className="add-book-input-details"
+              type="text"
+              name="title"
+              placeholder="Enter the title"
+              onChange={(e) => this.onChange(e)}
+            ></input>
+            <input
+              className="add-book-input-details"
+              type="text"
+              name="author"
+              placeholder="Enter the author"
+              onChange={(e) => this.onChange(e)}
+            ></input>
+          </div>
+          <div className="input-file-wrapper">
+            <label>Select image:</label>
+            <input
+              className="upload-bookimage"
+              type="file"
+              accept="image/*"
+              name="bookImage"
+              onChange={this.fileSelectedHandler}
+            ></input>
+          </div>
+          <div className="button-group">
+            <button className="btn-close">Cancel</button>
+            <button className="button-save" onClick={this.handleAddbook}>
+              Save
             </button>
-          }
-          position="left bottom"
-          arrow={false}
-        >
-          {(close) => (
-            <div className="popup-container">
-              <div className="add-book-header">
-                <h3>Add book</h3>
-              </div>
-              <div className="add-book-input-container">
-                <input
-                  className="add-book-input-details"
-                  type="text"
-                  name="title"
-                  placeholder="Enter the title"
-                  onChange={(e) => this.onChange(e)}
-                ></input>
-                <input
-                  className="add-book-input-details"
-                  type="text"
-                  name="author"
-                  placeholder="Enter the author"
-                  onChange={(e) => this.onChange(e)}
-                ></input>
-              </div>
-              <div className="input-file-wrapper">
-                <label>Select image:</label>
-                <input
-                  className="upload-bookimage"
-                  type="file"
-                  accept="image/*"
-                  name="bookImage"
-                  onChange={this.fileSelectedHandler}
-                ></input>
-              </div>
-              <div className="button-group">
-                <button className="btn-close" onClick={close}>
-                  Cancel
-                </button>
-                <button className="button-save" onClick={this.handleAddbook}>
-                  Save
-                </button>
-              </div>
-            </div>
-          )}
-        </Popup>
+          </div>
+        </div>
       </div>
     );
   }
